bc492156594798ef7c66752d7b20e971e488c23a
nandankundapura <nandankundapura777@gmail.com>
2025-08-22 10:20:29 +0530

    Add files via upload

---
+++ EvenOddCheck.java
@@ -0,0 +1,11 @@
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int num = 25; 
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even.");
+        } else {
+            System.out.println(num + " is Odd.");
+        }
+    }
+}
